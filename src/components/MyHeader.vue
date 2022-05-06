4e517237aa1f4b5c5dda0dfdd1ae5fb4d8b52248
Nurbolat <nurba.dav@gmail.com>
2022-05-06 21:24:26 +0600

    initial commit

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,34 @@
+<template>
+      <div class="headersection">
+        <div class="wrapper1">
+            <header class="header-top">
+                <a href="#" class="touhoulogo">
+                    <img class="logo12" src="img/bus.png">
+                </a>
+                <p class="shophame text-end">Bus Ticket KZ</p>
+                <nav>
+                    <ul class="nav nav-info" style="width: 950px">
+                        <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Schedule</a>
+                        </li>
+                        <li class="nav-item"><a href="tracking.html" class="nav-link">Tracking</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Help</a></li>
+                        <li class="nav-item"><a href="profile.html" class="nav-link">Profile</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Log out</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+        <button class="btn btn-primary">Button!</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    
+</style>
