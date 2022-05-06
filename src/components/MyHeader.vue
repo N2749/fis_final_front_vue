645d86d21256e1283a60d33562be42066d212ab5
Nurbolat <nurba.dav@gmail.com>
2022-05-06 21:59:01 +0600

    temp commit

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,16 +1,15 @@
 <template>
-      <div class="headersection">
+    <div class="headersection">
         <div class="wrapper1">
             <header class="header-top">
-                <a href="#" class="touhoulogo">
-                    <img class="logo12" src="img/bus.png">
+                <a href="#">
+                    <img class="logo12" src="../assets/bus.png">
                 </a>
                 <p class="shophame text-end">Bus Ticket KZ</p>
                 <nav>
                     <ul class="nav nav-info" style="width: 950px">
                         <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link">Schedule</a>
-                        </li>
+                        <li class="nav-item"><a href="#" class="nav-link">Schedule</a></li>
                         <li class="nav-item"><a href="tracking.html" class="nav-link">Tracking</a></li>
                         <li class="nav-item"><a href="#" class="nav-link">Help</a></li>
                         <li class="nav-item"><a href="profile.html" class="nav-link">Profile</a></li>
@@ -19,7 +18,6 @@
                 </nav>
             </header>
         </div>
-        <button class="btn btn-primary">Button!</button>
     </div>
 </template>
 
@@ -30,5 +28,49 @@ export default {
 </script>
 
 <style>
-    
+.headersection{
+	background: #04bf68;
+	display: flex;
+	-webkit-box-pack: justify;
+	justify-content: space-between;
+	color: white;
+}
+.wrapper1 {
+  max-width: 1110px;
+  margin: 0 auto;
+  display: grid;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.header-top {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: justify;
+      -ms-flex-pack: justify;
+          justify-content: space-between;
+  -webkit-box-align: center;
+      -ms-flex-align: center;
+          align-items: center;
+  padding-bottom: 10px;
+  padding-top: 10px;
+}
+.shophame{
+	margin: 0;
+	font-family: 'Bebas Neue', cursive;
+	display: flex;
+	color: white;
+	text-transform: uppercase;
+	justify-content: normal;
+	font-size: 20px;
+	width: 250px;
+}
+.nav-item .nav-link{
+	color: white;
+	text-decoration: none;
+	font-size: 20px;
+}
+.logo12{
+	width: 40px;
+}
 </style>
