092c167177488ed37cc9fd457030c74c40c86b25
n2749 <nurba.dav@gmail.com>
2022-05-09 14:25:30 +0600

    changing sources

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import MyHeader from './components/MyHeader.vue';
+import MyFooter from './components/MyFooter.vue';
 </script>
 
 <template>
   <MyHeader></MyHeader>
+  <MyFooter></MyFooter>
 </template>
 
 <style>
---
+++ src/components/MyFooter.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container">
+        <br>
+        <div class="row">
+            <div class="col-lg-3 col-sm-6">
+                <div class="infpart linkspart">
+                    <h2 class="footerh2">About us</h2>
+                    <p>On Bus Ticket Kz you can buy e-tickets for buses without leaving your home</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="infpart linkspart">
+                    <h2 class="footerh2">Main links</h2>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Tracking</a></li>
+                            <li><a href="">Schedule</a></li>
+                            <li><a href="">Help</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="infpart linkspart">
+                        <h2 class="footerh2">Questions</h2>
+                        <ul>
+                            <li><a href="">How to buy</a></li>
+                            <li><a href="">How to use the site</a></li>
+                            <li><a href="">What is an e-ticket</a></li>
+                            <li><a href="">How to refund</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="infpart linkspart">
+                        <h2 class="footerh2">Contact</h2>
+                        <p><strong>Company:</strong> Happy Angel<br><br>
+                            <strong>Address:</strong> Kazakhstan, Almaty, Abay, XXX<br><br>
+                            <strong>Contact Number:</strong> +7 (700) 000 00 01
+                        </p>
+                        <div class="social">
+                            <div class="social-icons icon-circle list-unstyled list-inline">
+                                <li> <a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-whatsapp"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-youtube-play"></i></a></li>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+            <p class="text-white text-center mb-4" style="color: white; ">Copyright © 2022.All rights reserved.</p>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container{
+	background: #232426;
+	padding-top: 20px;
+}
+
+</style>
