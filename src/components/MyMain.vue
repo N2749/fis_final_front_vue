f3c1e7f0c6539907ec014fd7117ab4c31d802993
solomon khan <hansolomon168@gmail.com>
2022-05-09 19:18:58 +0600

    does not work form

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,50 +1,33 @@
 <template>
   <main class="main">
-<<<<<<< HEAD
-    <MyFeatures></MyFeatures>
-=======
     <section class="buy-ticket">
       <div class="container buy-ticket__container">
         <h1 class="buy-ticket__title section-title">
           Go wherever you need to go
         </h1>
-        <MyTicketForm :stations="stations" @createTicketQuery="showListOfTickets"></MyTicketForm>
+        <MyTicketForm
+          :stations="stations"
+          @createTicketQuery="showListOfTickets"
+        ></MyTicketForm>
       </div>
     </section>
-<<<<<<< HEAD
-    <section class="features">
-      <MyFeatures></MyFeatures>
-=======
-    <section class="list">
-      <MyTicketsList :ticket-properties="ticketProperties"></MyTicketsList>
->>>>>>> 7a8fa046977697d5d05c87fe470e09ba2fbc0b71
-    </section>
->>>>>>> master
+    <MyFeatures></MyFeatures>
   </main>
 </template>
 
 <script>
 import MyTicketForm from './MyTicketForm.vue';
+import MyTicketsList from './MyTicketsList.vue';
 import MyFeatures from './MyFeatures.vue';
-export default {
-<<<<<<< HEAD
-  components: { MyFeatures },
-=======
-  components: { MyTicketsList, MyFeatures },
-=======
-import MyTicketForm from "./MyTicketForm.vue";
-import TicketList from "./TicketList.vue";
-import MyTicketsList from "./MyTicketsList.vue";
 
 export default {
-  components: {TicketList, MyTicketForm, MyTicketsList},
->>>>>>> master
+  components: { TicketList, MyTicketForm, MyTicketsList, MyFeatures },
   data() {
     return {
       ticketProperties: {
-        departurePoint: "",
-        destinationPoint: "",
-        departureDate: ""
+        departurePoint: '',
+        destinationPoint: '',
+        departureDate: '',
       },
       stations: [
         {
@@ -86,35 +69,23 @@ export default {
           name: 'Burabai',
           location: 'Burabai',
           id: 7,
-<<<<<<< HEAD
         },
       ],
     };
   },
-=======
-        }
-      ]
-    }
-  },
   methods: {
     showListOfTickets(ticketProperties) {
-      console.log(ticketProperties.target.value)
-    }
-  }
->>>>>>> f41d4555758966e1f1e88b843b8091c4aa709018
->>>>>>> master
+      console.log(ticketProperties.target.value);
+    },
+  },
 };
 </script>
 
 <style>
-<<<<<<< HEAD
-=======
-
 main {
   background-color: #8ebfb1;
 }
 
->>>>>>> master
 .buy-ticket {
   width: 100%;
   background-color: #8ebfb1;
