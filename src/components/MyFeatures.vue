dee4705f208e970c18c4475ec558a7e41d7edbd3
solomon khan <hansolomon168@gmail.com>
2022-05-09 19:05:35 +0600

    feat: finished features block

--- src/components/MyFeatures.vue
+++ src/components/MyFeatures.vue
@@ -1,32 +1,59 @@
 <template>
-  <div class="features__feature buy-ticket-online">
-    <div class="icon"></div>
-    <h2 class="features__title">Buying tickets online</h2>
-    <p class="features__text">
-      You can buy tickets without leaving your home. You can also return the
-      money.
-    </p>
-  </div>
-  <div class="features__feature tracking">
-    <div class="icon"></div>
-    <h2 class="features__title">Tracking</h2>
-    <p class="features__text">
-      You can find out the approximate location of the bus.
-    </p>
-  </div>
-  <div class="features__feature support">
-    <div class="icon"></div>
-    <h2 class="features__title">Best Support</h2>
-    <p class="features__text">
-      Fusce leo dui. Mauris et justo eget arcu ultricies porta. Nulla facilisi.
-      Nulla nec risus sit amet magna hendrerit venenatis. Sed porta tincidunt
-      lectus eget ultrices.
-    </p>
-  </div>
+  <section class="features">
+    <div class="features__feature buy-ticket-online">
+      <img src="./../assets/tickets.png" alt="tickets" />
+      <h2 class="features__title">Buying tickets online</h2>
+      <p class="features__text">
+        You can buy tickets without leaving your home. You can also return the
+        money.
+      </p>
+    </div>
+    <div class="features__feature tracking">
+      <img src="./../assets/tracking.png" alt="tracking" />
+      <h2 class="features__title">Tracking</h2>
+      <p class="features__text">
+        You can find out the approximate location of the bus.
+      </p>
+    </div>
+    <div class="features__feature support">
+      <img src="./../assets/support.png" alt="support" />
+      <h2 class="features__title">Best Support</h2>
+      <p class="features__text">
+        Fusce leo dui. Mauris et justo eget arcu ultricies porta. Nulla
+        facilisi.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.features {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 100px;
+}
+
+.features__feature {
+  flex-basis: 25%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  gap: 20px 0;
+  padding: 20px;
+  background-color: #8ebfb1;
+  border: none;
+  border-radius: 5px;
+  color: white;
+}
+
+.features__text {
+  font-size: 16px;
+  text-align: justify;
+}
+</style>
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,84 +1,64 @@
 <template>
   <main class="main">
-    <section class="buy-ticket">
-      <div class="container buy-ticket__container">
-        <h1 class="buy-ticket__title section-title">
-          Go wherever you need to go
-        </h1>
-        <MyTicketForm :stations="stations"></MyTicketForm>
-      </div>
-    </section>
-    <section class="features">
-      <MyFeatures></MyFeatures>
-    </section>
+    <MyFeatures></MyFeatures>
   </main>
 </template>
 
 <script>
-<<<<<<< HEAD
+import MyTicketForm from './MyTicketForm.vue';
 import MyFeatures from './MyFeatures.vue';
-import MyTicketsList from './MyTicketsList.vue';
 export default {
-  components: { MyTicketsList, MyFeatures },
-=======
-import MyTicketForm from "./MyTicketForm.vue";
-import TicketList from "./TicketList.vue";
-
-export default {
-  components: {TicketList, MyTicketForm},
+  components: { MyFeatures },
   data() {
     return {
       stations: [
         {
-          name: "Taraz",
-          location: "Taraz",
+          name: 'Taraz',
+          location: 'Taraz',
           id: 0,
         },
         {
-          name: "Ust-Kamenogorsk",
-          location: "Ust-Kamenogorsk",
+          name: 'Ust-Kamenogorsk',
+          location: 'Ust-Kamenogorsk',
           id: 1,
         },
         {
-          name: "Shymkent",
-          location: "Shymkent",
+          name: 'Shymkent',
+          location: 'Shymkent',
           id: 2,
         },
         {
-          name: "Taldykorgan",
-          location: "Taldykorgan",
+          name: 'Taldykorgan',
+          location: 'Taldykorgan',
           id: 3,
         },
         {
-          name: "Semei",
-          location: "Semei",
+          name: 'Semei',
+          location: 'Semei',
           id: 4,
         },
         {
-          name: "Nur-Sultan",
-          location: "Nur-Sultan",
+          name: 'Nur-Sultan',
+          location: 'Nur-Sultan',
           id: 5,
         },
         {
-          name: "Karaganda",
-          location: "Karaganda",
+          name: 'Karaganda',
+          location: 'Karaganda',
           id: 6,
         },
         {
-          name: "Burabai",
-          location: "Burabai",
+          name: 'Burabai',
+          location: 'Burabai',
           id: 7,
-        }
-      ]
-    }
-  }
->>>>>>> f41d4555758966e1f1e88b843b8091c4aa709018
+        },
+      ],
+    };
+  },
 };
-
 </script>
 
 <style>
-
 .buy-ticket {
   width: 100%;
   background-color: #8ebfb1;
@@ -90,5 +70,4 @@ export default {
   align-items: center;
   gap: 30px 0;
 }
-
 </style>
